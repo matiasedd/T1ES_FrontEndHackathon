5405f2a6763b2c6e114a0ef1eb1f90821c946876
Edison Matias <edison.matias@ufpr.br>
2026-09-08 13:01:31 -0300

    update (router): added POST http method and params

--- router.py
+++ router.py
@@ -1,8 +1,11 @@
-from datetime import datetime
 import requests
 
 BASE_PATH = "http://localhost:8000"
 
-def get(endpoint: str):
-    response = requests.get(f"{BASE_PATH}{endpoint}")
+def get(endpoint: str, params=None):
+    response = requests.get(f"{BASE_PATH}{endpoint}", params=params)
+    return response.json()
+
+def post(endpoint: str, data=None):
+    response = requests.post(f"{BASE_PATH}{endpoint}", json=data)
     return response.json()
